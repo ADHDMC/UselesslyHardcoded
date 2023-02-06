4866fc53b2737bb1a37e7158ca275b93f46e5e5c
RhythmicSys <mc.adhdmc@gmail.com>
2023-02-05 19:13:04 -0800

    fix minecraft buzz' display name

--- src/main/java/adhdmc/uselesslyhardcoded/commands/VoteCommand.java
+++ src/main/java/adhdmc/uselesslyhardcoded/commands/VoteCommand.java
@@ -15,7 +15,10 @@ public class VoteCommand implements CommandExecutor {
                                   <click:open_url:'https://minecraft-mp.com/server/313337/vote/'><hover:show_text:'<aqua>https://minecraft-mp.com/server/313337/vote/'><gray>• <#519efc>Minecraft-MP</hover></click><reset>
                                   <click:open_url:'https://minecraftservers.org/vote/644666/'><hover:show_text:'<aqua>https://minecraftservers.org/vote/644666/'><gray>• <#519efc>MinecraftServers</hover></click><reset>
                                   <click:open_url:'https://minecraft-server-list.com/server/492776/vote/'><hover:show_text:'<aqua>https://minecraft-server-list.com/server/492776/vote/'><gray>• <#519efc>Minecraft Server List</hover></click><reset>
-                                  <yellow>Rewards for each vote are <aqua>❖ 4 Diamonds</aqua> and <green>◎ 10 Credits""");
+                                  <click:open_url:'https://minecraft.buzz/vote/6869'><hover:show_text:'<aqua>https://minecraft.buzz/vote/6869'><gray>• <#519efc>Minecraft Buzz</hover></click><reset>
+                                  <click:open_url:'https://servers-minecraft.net/server-creatorssmp.23554'><hover:show_text:'<aqua>https://servers-minecraft.net/server-creatorssmp.23554'><gray>• <#519efc>Servers-Minecraft</hover></click><reset>
+                                  <yellow>Rewards for each vote are <aqua>❖ 4 Diamonds</aqua> and <green>◎ 10 Credits
+                                  <gray>Note: Be sure to keep the tabs open until the site says successful, if you close the tabs before the vote goes through, your rewards may not properly register.""");
         return true;
     }
 }
